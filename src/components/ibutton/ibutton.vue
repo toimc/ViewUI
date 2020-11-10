40e66d834f7486b5782ac4c6d84fb327d6576b7a
brian <brian@toimc.com>
2020-11-10 12:16:40 +0800

    添加eslint相关依赖

--- src/components/ibutton/ibutton.vue
+++ src/components/ibutton/ibutton.vue
@@ -1,122 +1,132 @@
 <template>
-    <component :is="tagName" :class="classes" :disabled="itemDisabled" @click="handleClickLink" v-bind="tagProps">
-        <Icon class="ivu-load-loop" type="ios-loading" v-if="loading"></Icon>
-        <Icon :type="icon" :custom="customIcon" v-if="(icon || customIcon) && !loading"></Icon>
-        <span v-if="showSlot" ref="slot"><slot></slot></span>
-    </component>
+  <component
+    :is="tagName"
+    :class="classes"
+    :disabled="itemDisabled"
+    @click="handleClickLink"
+    v-bind="tagProps"
+  >
+    <Icon class="ivu-load-loop" type="ios-loading" v-if="loading"></Icon>
+    <Icon
+      :type="icon"
+      :custom="customIcon"
+      v-if="(icon || customIcon) && !loading"
+    ></Icon>
+    <span v-if="showSlot" ref="slot"><slot></slot></span>
+  </component>
 </template>
 
 <script>
-    import Icon from '../icon';
-    import { oneOf } from '../../utils/assist';
-    import mixinsLink from '../../mixins/link';
-    import mixinsForm from '../../mixins/form';
-    import { getCurrentInstance } from 'vue';
+import Icon from '../icon'
+import { oneOf } from '../../utils/assist'
+import mixinsLink from '../../mixins/link'
+import mixinsForm from '../../mixins/form'
+import { getCurrentInstance } from 'vue'
 
-    const prefixCls = 'ivu-btn';
+const prefixCls = 'ivu-btn'
 
-    export default {
-        name: 'iButton',
-        mixins: [ mixinsLink, mixinsForm ],
-        components: { Icon },
-        props: {
-            type: {
-                validator (value) {
-                    return oneOf(value, ['default', 'primary', 'dashed', 'text', 'info', 'success', 'warning', 'error']);
-                },
-                default: 'default'
-            },
-            shape: {
-                validator (value) {
-                    return oneOf(value, ['circle', 'circle-outline']);
-                }
-            },
-            size: {
-                validator (value) {
-                    return oneOf(value, ['small', 'large', 'default']);
-                },
-                default () {
-                    const internalInstance = getCurrentInstance();
-                    const config = internalInstance.appContext.config;
-                    return !config.$IVIEW || config.$IVIEW.size === '' ? 'default' : config.$IVIEW.size;
-                }
-            },
-            loading: Boolean,
-            disabled: {
-                default: false,
-                type: Boolean
-            },
-            htmlType: {
-                default: 'button',
-                validator (value) {
-                    return oneOf(value, ['button', 'submit', 'reset']);
-                }
-            },
-            icon: {
-                type: String,
-                default: ''
-            },
-            customIcon: {
-                type: String,
-                default: ''
-            },
-            long: {
-                type: Boolean,
-                default: false
-            },
-            ghost: {
-                type: Boolean,
-                default: false
-            }
-        },
-        computed: {
-            showSlot () {
-                // Was this
-                // return !!this.$slots.default;
-                // Should become???
-                // return !!this.$slots.default();
-                return true;
-            },
-            classes () {
-                return [
+export default {
+  name: 'iButton',
+  mixins: [mixinsLink, mixinsForm],
+  components: { Icon },
+  props: {
+    type: {
+      validator (value) {
+        return oneOf(value, ['default', 'primary', 'dashed', 'text', 'info', 'success', 'warning', 'error'])
+      },
+      default: 'default'
+    },
+    shape: {
+      validator (value) {
+        return oneOf(value, ['circle', 'circle-outline'])
+      }
+    },
+    size: {
+      validator (value) {
+        return oneOf(value, ['small', 'large', 'default'])
+      },
+      default () {
+        const internalInstance = getCurrentInstance()
+        const config = internalInstance.appContext.config
+        return !config.$IVIEW || config.$IVIEW.size === '' ? 'default' : config.$IVIEW.size
+      }
+    },
+    loading: Boolean,
+    disabled: {
+      default: false,
+      type: Boolean
+    },
+    htmlType: {
+      default: 'button',
+      validator (value) {
+        return oneOf(value, ['button', 'submit', 'reset'])
+      }
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
+    customIcon: {
+      type: String,
+      default: ''
+    },
+    long: {
+      type: Boolean,
+      default: false
+    },
+    ghost: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    showSlot () {
+      // Was this
+      // return !!this.$slots.default;
+      // Should become???
+      // return !!this.$slots.default();
+      return true
+    },
+    classes () {
+      return [
                     `${prefixCls}`,
                     `${prefixCls}-${this.type}`,
                     {
-                        [`${prefixCls}-long`]: this.long,
-                        [`${prefixCls}-${this.shape}`]: !!this.shape,
-                        [`${prefixCls}-${this.size}`]: this.size !== 'default',
-                        [`${prefixCls}-loading`]: this.loading != null && this.loading,
-                        [`${prefixCls}-icon-only`]: !this.showSlot && (!!this.icon || !!this.customIcon || this.loading),
-                        [`${prefixCls}-ghost`]: this.ghost
+                      [`${prefixCls}-long`]: this.long,
+                      [`${prefixCls}-${this.shape}`]: !!this.shape,
+                      [`${prefixCls}-${this.size}`]: this.size !== 'default',
+                      [`${prefixCls}-loading`]: this.loading != null && this.loading,
+                      [`${prefixCls}-icon-only`]: !this.showSlot && (!!this.icon || !!this.customIcon || this.loading),
+                      [`${prefixCls}-ghost`]: this.ghost
                     }
-                ];
-            },
-            // Point out if it should render as <a> tag
-            isHrefPattern() {
-                return !!this.to;
-            },
-            tagName() {
-                return this.isHrefPattern ? 'a' : 'button';
-            },
-            tagProps() {
-                const {isHrefPattern} = this;
-                if(isHrefPattern) {
-                    const {linkUrl,target}=this;
-                    return {href: linkUrl, target};
-                } else {
-                    const {htmlType} = this;
-                    return {type: htmlType};
-                }
-            }
-        },
-        methods: {
-            // Ctrl or CMD and click, open in new window when use `to`
-            handleClickLink (event) {
-                this.$emit('click', event);
-                const openInNewWindow = event.ctrlKey || event.metaKey;
+      ]
+    },
+    // Point out if it should render as <a> tag
+    isHrefPattern () {
+      return !!this.to
+    },
+    tagName () {
+      return this.isHrefPattern ? 'a' : 'button'
+    },
+    tagProps () {
+      const { isHrefPattern } = this
+      if (isHrefPattern) {
+        const { linkUrl, target } = this
+        return { href: linkUrl, target }
+      } else {
+        const { htmlType } = this
+        return { type: htmlType }
+      }
+    }
+  },
+  methods: {
+    // Ctrl or CMD and click, open in new window when use `to`
+    handleClickLink (event) {
+      this.$emit('click', event)
+      const openInNewWindow = event.ctrlKey || event.metaKey
 
-                this.handleCheckClick(event, openInNewWindow);
-            }
-        }
-    };
+      this.handleCheckClick(event, openInNewWindow)
+    }
+  }
+}
 </script>
